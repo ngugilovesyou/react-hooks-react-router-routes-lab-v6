a2abdd31e224150f06ec7b11f468078ac7f64b43
samuelgitau <amsamgitau@gmail.com>
2024-08-18 16:29:55 +0300

    complete lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import Card from "../components/Card";
 
 function Actors() {
+  const [actors, setActors] = useState([])
+  useEffect(() =>{
+    fetch("http://localhost:4000/actors")
+    .then(r => r.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+  const actorList = actors.map(actor => <Card key={actor.id} name={actor.name} movies={actor.movies} />)
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actorList}
       </main>
     </>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,29 @@
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import ErrorPage from "./pages/ErrorPages";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
 
 
 const routes = [
-   
+  {
+    path:("/"),
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path:("/directors"),
+    element:<Directors />
+  },
+  {
+    path:("/actors"),
+    element:<Actors />
+  },
+  {
+    path:("/movie/:id"),
+    element:<Movie />
+  }
+
   ];
 
 export default routes;
